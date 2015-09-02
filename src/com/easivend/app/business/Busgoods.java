f485a2e63a7086412a93378ac25583aeded74511
郭阵阵 <123338121@qq.com>
2015-09-02 15:53:11 +0800

    商品详细页面横屏

--- src/com/easivend/app/business/Busgoods.java
+++ src/com/easivend/app/business/Busgoods.java
@@ -63,21 +63,21 @@ public class Busgoods extends Activity
 		BusgoodsAct = this;
 		this.gvbusgoodsProduct=(GridView) findViewById(R.id.gvbusgoodsProduct);
 		this.imgbtnbusgoodsback=(ImageButton)findViewById(R.id.imgbtnbusgoodsback);
-		//动态设置控件高度
-    	//
-    	DisplayMetrics  dm = new DisplayMetrics();  
-        //取得窗口属性  
-        getWindowManager().getDefaultDisplay().getMetrics(dm);  
-        //窗口的宽度  
-        int screenWidth = dm.widthPixels;          
-        //窗口高度  
-        int screenHeight = dm.heightPixels;      
-        ToolClass.Log(ToolClass.INFO,"EV_JNI","APP<<屏幕"+screenWidth
-				+"],["+screenHeight+"]","log.txt");	
-		
-    	LinearLayout.LayoutParams linearParams = (LinearLayout.LayoutParams) gvbusgoodsProduct.getLayoutParams(); // 取控件mGrid当前的布局参数
-    	linearParams.height =  screenHeight-170;// 当控件的高强制设成75象素
-    	gvbusgoodsProduct.setLayoutParams(linearParams); // 使设置好的布局参数应用到控件mGrid2
+//		//动态设置控件高度
+//    	//
+//    	DisplayMetrics  dm = new DisplayMetrics();  
+//        //取得窗口属性  
+//        getWindowManager().getDefaultDisplay().getMetrics(dm);  
+//        //窗口的宽度  
+//        int screenWidth = dm.widthPixels;          
+//        //窗口高度  
+//        int screenHeight = dm.heightPixels;      
+//        ToolClass.Log(ToolClass.INFO,"EV_JNI","APP<<屏幕"+screenWidth
+//				+"],["+screenHeight+"]","log.txt");	
+//		
+//    	LinearLayout.LayoutParams linearParams = (LinearLayout.LayoutParams) gvbusgoodsProduct.getLayoutParams(); // 取控件mGrid当前的布局参数
+//    	linearParams.height =  screenHeight-170;// 当控件的高强制设成75象素
+//    	gvbusgoodsProduct.setLayoutParams(linearParams); // 使设置好的布局参数应用到控件mGrid2
     	//从商品分类页面中取得锁选中的商品类型
 		Intent intent=getIntent();
 		Bundle bundle=intent.getExtras();
