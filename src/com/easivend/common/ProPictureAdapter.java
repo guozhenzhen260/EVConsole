eb7d276368d76835255e38f17b1c344db932ea34
guozhenzhen260 <123338121@qq.com>
2016-06-06 11:11:27 +0800

    修改漏洞：无图片时，商品数量不显示

--- src/com/easivend/common/ProPictureAdapter.java
+++ src/com/easivend/common/ProPictureAdapter.java
@@ -47,7 +47,7 @@ public class ProPictureAdapter extends BaseAdapter {// 创建基于BaseAdapter的子类
         {        	
             ProPicture picture = new ProPicture(proID[i],promarket[i],prosales[i], proImage[i],procount[i]);// 使用标题和图像生成ProPicture对象
             pictures.add(picture);// 将Picture对象添加到泛型集合中
-            ToolClass.Log(ToolClass.INFO,"EV_JNI","APP<<proID="+proID[i]+",promarket="+promarket[i]+",prosales="+prosales[i]+",proImage="+proImage[i]+",procount="+procount[i],"log.txt");
+            ToolClass.Log(ToolClass.INFO,"EV_JNI","APP<<proID="+picture.getProID()+",promarket="+picture.getPromarket()+",prosales="+picture.getProsales()+",proImage="+picture.getProImage()+",procount="+picture.getProcount(),"log.txt");
         }
     }
 
@@ -95,8 +95,28 @@ public class ProPictureAdapter extends BaseAdapter {// 创建基于BaseAdapter的子类
         viewHolder.promarket.setText("原价:"+pictures.get(arg0).getPromarket());// 设置原价
         viewHolder.promarket.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG); //删除线
         viewHolder.prosales.setText("现价:"+pictures.get(arg0).getProsales());// 设置现价
-        ToolClass.Log(ToolClass.INFO,"EV_JNI","商品:"+pictures.get(arg0).getProID()+"Img2="+pictures.get(arg0).getProImage(),"log.txt");
-        //有对应的商品id
+        //显示商品数量
+		if(Integer.parseInt(pictures.get(arg0).getProcount())>0)
+        {
+        	viewHolder.count.setText("剩余数量:"+pictures.get(arg0).getProcount());// 设置剩余数量
+        	viewHolder.proID.setTextColor(android.graphics.Color.BLACK);
+        	viewHolder.count.setTextColor(android.graphics.Color.BLACK);
+        	viewHolder.promarket.setTextColor(android.graphics.Color.BLACK);
+        	viewHolder.prosales.setTextColor(android.graphics.Color.BLACK);		        	
+        }
+        else
+        {
+        	viewHolder.count.setText("剩余数量:已售罄");// 设置剩余数量
+        	viewHolder.proID.setTextColor(android.graphics.Color.GRAY);
+        	viewHolder.count.setTextColor(android.graphics.Color.GRAY);
+        	viewHolder.promarket.setTextColor(android.graphics.Color.GRAY);
+        	viewHolder.prosales.setTextColor(android.graphics.Color.GRAY);		        	
+        }
+        ToolClass.Log(ToolClass.INFO,"EV_JNI","商品:"+pictures.get(arg0).getProID()+",promarket="+pictures.get(arg0).getPromarket()+",prosales="+pictures.get(arg0).getProsales()+",proImage="+pictures.get(arg0).getProImage()+",procount="+pictures.get(arg0).getProcount(),"log.txt");
+        //*********
+        //商品图片显示
+        //*********
+        //有对应的商品图片
         if((pictures.get(arg0).getProImage()!=null)&&(pictures.get(arg0).getProImage().equals("0")!=true)&&(pictures.get(arg0).getProImage().equals("")!=true))
         {
         	String ATT_ID="";
@@ -106,39 +126,20 @@ public class ProPictureAdapter extends BaseAdapter {// 创建基于BaseAdapter的子类
     			ATT_ID=a[a.length-1];
     			ATT_ID=ATT_ID.substring(0,ATT_ID.lastIndexOf("."));
     			ToolClass.Log(ToolClass.INFO,"EV_JNI","图片ATT_ID="+ATT_ID,"log.txt");
-    		}
+    		}    		
         	ToolClass.Log(ToolClass.INFO,"EV_JNI","APP<<图片pro="+pictures.get(arg0).getProID()+",addr="+pictures.get(arg0).getProImage()+",ATT_ID="+ATT_ID,"log.txt");
+        	//图片没有下载下来
         	if(ToolClass.isImgFile(ATT_ID)==false)
 			{
 				ToolClass.Log(ToolClass.INFO,"EV_JNI","商品["+pictures.get(arg0).getProID()+"]图片不存在","log.txt");
-				//显示商品信息
-				if(Integer.parseInt(pictures.get(arg0).getProcount())>0)
-		        {
-		        	viewHolder.count.setText("剩余数量:"+pictures.get(arg0).getProcount());// 设置剩余数量
-		        	viewHolder.proID.setTextColor(android.graphics.Color.BLACK);
-		        	viewHolder.count.setTextColor(android.graphics.Color.BLACK);
-		        	viewHolder.promarket.setTextColor(android.graphics.Color.BLACK);
-		        	viewHolder.prosales.setTextColor(android.graphics.Color.BLACK);		        	
-		        }
-		        else
-		        {
-		        	viewHolder.count.setText("剩余数量:已售罄");// 设置剩余数量
-		        	viewHolder.proID.setTextColor(android.graphics.Color.GRAY);
-		        	viewHolder.count.setTextColor(android.graphics.Color.GRAY);
-		        	viewHolder.promarket.setTextColor(android.graphics.Color.GRAY);
-		        	viewHolder.prosales.setTextColor(android.graphics.Color.GRAY);		        	
-		        }
+				viewHolder.image.setImageResource(R.drawable.wutupian);
 			}
+        	//图片有下载下来
 			else 
 			{
 				ToolClass.Log(ToolClass.INFO,"EV_JNI","商品["+pictures.get(arg0).getProID()+"]显示图片","log.txt");
 	        	if(Integer.parseInt(pictures.get(arg0).getProcount())>0)
 		        {
-		        	viewHolder.count.setText("剩余数量:"+pictures.get(arg0).getProcount());// 设置剩余数量
-		        	viewHolder.proID.setTextColor(android.graphics.Color.BLACK);
-		        	viewHolder.count.setTextColor(android.graphics.Color.BLACK);
-		        	viewHolder.promarket.setTextColor(android.graphics.Color.BLACK);
-		        	viewHolder.prosales.setTextColor(android.graphics.Color.BLACK);
 		        	/*为什么图片一定要转化为 Bitmap格式的！！ */
 		            Bitmap bitmap = ToolClass.getLoacalBitmap(pictures.get(arg0).getProImage()); //从本地取图片(在cdcard中获取)  //
 		            if(bitmap!=null)
@@ -146,11 +147,6 @@ public class ProPictureAdapter extends BaseAdapter {// 创建基于BaseAdapter的子类
 		        }
 		        else
 		        {
-		        	viewHolder.count.setText("剩余数量:已售罄");// 设置剩余数量
-		        	viewHolder.proID.setTextColor(android.graphics.Color.GRAY);
-		        	viewHolder.count.setTextColor(android.graphics.Color.GRAY);
-		        	viewHolder.promarket.setTextColor(android.graphics.Color.GRAY);
-		        	viewHolder.prosales.setTextColor(android.graphics.Color.GRAY);
 		        	/*原图片加载已售完水印 */
 		            Bitmap photo = ToolClass.getLoacalBitmap(pictures.get(arg0).getProImage()); //从本地取图片(在cdcard中获取)  //
 		            if(photo!=null)
