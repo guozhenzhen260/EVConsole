49ae7b0af9ddb687ce592ea8b76e8787af762ef0
guozhenzhen260 <123338121@qq.com>
2016-09-05 16:41:48 +0800

    读卡器测试页面

--- src/com/easivend/app/maintain/CahslessTest.java
+++ src/com/easivend/app/maintain/CahslessTest.java
@@ -1,17 +1,36 @@
 package com.easivend.app.maintain;
 
+import com.easivend.common.ToolClass;
 import com.example.evconsole.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CahslessTest extends Activity {
-
+	private TextView txtcashlesstest=null;
+	private EditText edtcashlesstest=null;
+	private Button btncashlesstestopen=null,btncashlesstestok=null,btncashlesstestquery=null
+			,btncashlesstestclose=null,btncashlesstestcancel=null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.cashlesstest);	
+		//设置横屏还是竖屏的布局策略
+		this.setRequestedOrientation(ToolClass.getOrientation());
+		//退出
+		btncashlesstestcancel = (Button)findViewById(R.id.btncashlesstestcancel);		
+		btncashlesstestcancel.setOnClickListener(new OnClickListener() {
+		    @Override
+		    public void onClick(View arg0) {		    	
+		    	finish();
+		    }
+		});
 	}
 
 }
