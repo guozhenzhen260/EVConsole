d0d626b92a5a39e8e64326b0d019098327b8d973
guozhenzhen260 <123338121@qq.com>
2016-10-28 14:18:24 +0800

    竖屏完成扣款成功，扣款失败获取卡余额额

--- src/com/easivend/app/business/BusPort.java
+++ src/com/easivend/app/business/BusPort.java
@@ -65,6 +65,7 @@ import com.landfoneapi.mispos.ErrCode;
 import com.landfoneapi.mispos.LfMISPOSApi;
 import com.landfoneapi.protocol.pkg.REPLY;
 import com.landfoneapi.protocol.pkg._04_GetRecordReply;
+import com.landfoneapi.protocol.pkg._04_QueryReply;
 import com.printsdk.cmd.PrintCmd;
 
 import android.annotation.SuppressLint;
@@ -178,6 +179,8 @@ BushuoFragInteraction
     private LfMISPOSApi mMyApi = new LfMISPOSApi();
     private Handler posmainhand=null;
     private int iszhipos=0;//1成功发送了扣款请求,0没有发送成功扣款请求，2刷卡扣款已经完成并且金额足够
+    //查询参数
+  	private String cashbalance = "";
     //退款参数
    	private String rfd_card_no = "";
    	private String rfd_spec_tmp_serial = "";
@@ -673,13 +676,22 @@ BushuoFragInteraction
 						}, 300);
 						break;
 					case CahslessTest.COSTFAIL:	
-						listterner.BusportTsxx("提示信息：扣款失败");
+						listterner.BusportTsxx("提示信息：扣款失败"+cashbalance);
 						iszhipos=0;
 						break;
 					case CahslessTest.QUERYSUCCESS:
 					case CahslessTest.QUERYFAIL:	
-						//listterner.BusportTsxx("单据信息："+SpecInfoField);
-						tochuhuo();	
+						listterner.BusportTsxx("提示信息：卡余额"+cashbalance);
+						//延时
+					    new Handler().postDelayed(new Runnable() 
+						{
+				            @Override
+				            public void run() 
+				            {   
+								tochuhuo();	
+							}
+
+						}, 3000);
 						break;
 					case CahslessTest.DELETESUCCESS:
 					case CahslessTest.DELETEFAIL:	
@@ -1370,53 +1382,99 @@ BushuoFragInteraction
   				//info(rst.op + ":" + rst.code + "," + rst.code_info);
   				//【操作标识符】LfMISPOSApi下“OP_”开头的静态变量，如：LfMISPOSApi.OP_INIT、LfMISPOSApi.OP_PURCHASE等等
   				//打开串口
-  				if(rst.op.equals(LfMISPOSApi.OP_INIT))
-  				{
-  					//【返回码和信息】code和code_info的返回/说明，见com.landfoneapi.mispos.ErrCode
-  					if(rst.code.equals(ErrCode._00.getCode())){//返回00，代表成功
-  						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 打开成功"+ToolClass.getCardcom(),"com.txt");
-  						childmsg.what=CahslessTest.OPENSUCCESS;
-  						childmsg.obj="打开成功"+ToolClass.getCardcom();
-  					}else{
-  						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 打开失败"+ToolClass.getCardcom()+",code:"+rst.code+",info:"+rst.code_info,"com.txt");						
-  						childmsg.what=CahslessTest.OPENFAIL;
-  						childmsg.obj="打开失败"+ToolClass.getCardcom()+",code:"+rst.code+",info:"+rst.code_info;
-  					}
-  				}
-  				//关闭串口
-  				else if(rst.op.equals(LfMISPOSApi.OP_RELEASE))
-  				{
-  					if(rst.code.equals(ErrCode._00.getCode())){//返回00，代表成功
-  						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 关闭成功","com.txt");
-  						childmsg.what=CahslessTest.CLOSESUCCESS;
-  						childmsg.obj="关闭成功";
-  					}else{
-  						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 关闭失败,code:"+rst.code+",info:"+rst.code_info,"com.txt");						
-  						childmsg.what=CahslessTest.CLOSEFAIL;
-  						childmsg.obj="关闭失败,code:"+rst.code+",info:"+rst.code_info;
-  					}
-  				}
-  				//扣款
-  				else if(rst.op.equals(LfMISPOSApi.OP_PURCHASE))
-  				{
-  					if(rst.code.equals(ErrCode._00.getCode())){//返回00，代表成功
-  						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 扣款成功","com.txt");
-  						childmsg.what=CahslessTest.COSTSUCCESS;
-  						childmsg.obj="扣款成功";
-  					}
-  					else if(rst.code.equals(ErrCode._XY.getCode())){
+				if(rst.op.equals(LfMISPOSApi.OP_INIT))
+				{
+					//【返回码和信息】code和code_info的返回/说明，见com.landfoneapi.mispos.ErrCode
+					if(rst.code.equals(ErrCode._00.getCode())){//返回00，代表成功
+						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 打开成功"+ToolClass.getCardcom(),"com.txt");
+						childmsg.what=CahslessTest.OPENSUCCESS;
+						childmsg.obj="打开成功"+ToolClass.getCardcom();
+					}else{
+						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 打开失败"+ToolClass.getCardcom()+",code:"+rst.code+",info:"+rst.code_info,"com.txt");						
+						childmsg.what=CahslessTest.OPENFAIL;
+						childmsg.obj="打开失败"+ToolClass.getCardcom()+",code:"+rst.code+",info:"+rst.code_info;
+					}
+				}
+				//关闭串口
+				else if(rst.op.equals(LfMISPOSApi.OP_RELEASE))
+				{
+					if(rst.code.equals(ErrCode._00.getCode())){//返回00，代表成功
+						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 关闭成功","com.txt");
+						childmsg.what=CahslessTest.CLOSESUCCESS;
+						childmsg.obj="关闭成功";
+					}else{
+						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 关闭失败,code:"+rst.code+",info:"+rst.code_info,"com.txt");						
+						childmsg.what=CahslessTest.CLOSEFAIL;
+						childmsg.obj="关闭失败,code:"+rst.code+",info:"+rst.code_info;
+					}
+				}
+				//签到
+				else if (rst.op.equals(LfMISPOSApi.OP_SIGNIN)) 
+				{
+                    if (rst.code.equals(ErrCode._00.getCode())) {//返回00，代表成功
+                    	ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 签到成功","com.txt");
+                    } else {
+                        ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 签到失败,code:"+rst.code+",info:"+rst.code_info,"com.txt");						
+                    }
+                }
+				//结算【结算时间可能会非常长】
+				else if (rst.op.equals(LfMISPOSApi.OP_SETTLE)) 
+				{
+                    if (rst.code.equals(ErrCode._00.getCode())) {//返回00，代表成功
+                    	ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 结算成功","com.txt");                        
+                    } else {
+                    	ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 结算失败,code:"+rst.code+",info:"+rst.code_info,"com.txt");		
+                    }
+                } 
+				//扣款
+				else if(rst.op.equals(LfMISPOSApi.OP_PURCHASE))
+				{
+					if(rst.code.equals(ErrCode._00.getCode())){//返回00，代表成功
+						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 扣款成功","com.txt");
+						childmsg.what=CahslessTest.COSTSUCCESS;
+						childmsg.obj="扣款成功";
+					}
+					else if(rst.code.equals(ErrCode._XY.getCode())){
   						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 撤销成功","com.txt");
   						childmsg.what=CahslessTest.DELETESUCCESS;
   						childmsg.obj="撤销成功";
   					}
-  					else
-  					{
-  						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 扣款失败,code:"+rst.code+",info:"+rst.code_info,"com.txt");
-  						childmsg.what=CahslessTest.COSTFAIL;
-  						childmsg.obj="扣款失败,code:"+rst.code+",info:"+rst.code_info;
-  					}
-  				}
-  				//退款
+					else{
+						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 扣款失败,code:"+rst.code+",info:"+rst.code_info,"com.txt");
+						if(rst.code.equals("01"))
+						{
+							cashbalance=rst.code_info;
+						}
+						childmsg.what=CahslessTest.COSTFAIL;
+						childmsg.obj="扣款失败,code:"+rst.code+",info:"+rst.code_info;
+					}
+				}
+				//查询
+				else if (rst.op.equals(LfMISPOSApi.OP_QUERY)) 
+				{
+                    if (rst.code.equals(ErrCode._00.getCode()))
+                    {
+                        String tmp = "查询结果:";
+                        tmp += "\n错误代码：" + ((_04_QueryReply) (rst)).getCode();
+                        tmp += ",\n提示信息：" + ((_04_QueryReply) (rst)).getCode_info();
+                        tmp += ",\n卡号:" + ((_04_QueryReply) (rst)).getCardNo();//卡号
+                        tmp += ",\n现金余额:" + ((_04_QueryReply) (rst)).getCashBalance();//现金余额
+                        tmp += ",\n积分余额:" + ((_04_QueryReply) (rst)).getPointBalance();//积分余额
+                        ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 查询成功="+tmp,"com.txt");
+                        //查询参数获取
+                        cashbalance = ((_04_QueryReply) (rst)).getCashBalance();
+                        childmsg.what=CahslessTest.FINDSUCCESS;
+						childmsg.obj="查询成功="+tmp;
+                        
+                    } 
+                    else 
+                    {
+                    	ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 查询失败,code:"+rst.code+",info:"+rst.code_info,"com.txt");
+                        childmsg.what=CahslessTest.FINDFAIL;
+						childmsg.obj="查询失败,code:"+rst.code+",info:"+rst.code_info;
+                    }
+                } 
+				//退款
   				else if(rst.op.equals(LfMISPOSApi.OP_REFUND))
   				{
   					//返回00，代表成功
@@ -1432,40 +1490,40 @@ BushuoFragInteraction
 						childmsg.obj="退款失败,code:"+rst.code+",info:"+rst.code_info;
 					}
 				}
-  				//返回结果
-  				else if(rst.op.equals(LfMISPOSApi.OP_GETRECORD))
-  				{
-  					//返回00，代表成功
-  					if(rst.code.equals(ErrCode._00.getCode()))
-  					{
-  						String tmp = "单据:特定信息=";
-  						tmp += "[" + ((_04_GetRecordReply) (rst)).getSpecInfoField();//特定信息【会员卡需要！！】
-  						/*特定信息说明
-  						+储值卡号(19)
-  						+终端流水号(6)
-  						+终端编号(8)
-  						+批次号(6)
-  						+商户号(15)
-  						+商户名称(60)
-  						+会员名称(60)
-  						+交易时间(6)
-  						+交易日期(8)
-  						+交易单号(14)
-  						+消费金额(12)
-  						+账户余额(12)
-  						+临时交易流水号（26）
-  						以上都是定长，金额都是定长12位，前补0，其他不足位数后补空格
+				//返回结果
+				else if(rst.op.equals(LfMISPOSApi.OP_GETRECORD))
+				{
+					//返回00，代表成功
+					if(rst.code.equals(ErrCode._00.getCode()))
+					{
+						String tmp = "单据:特定信息=";
+						tmp += "[" + ((_04_GetRecordReply) (rst)).getSpecInfoField();//特定信息【会员卡需要！！】
+						/*特定信息说明
+						+储值卡号(19)
+						+终端流水号(6)
+						+终端编号(8)
+						+批次号(6)
+						+商户号(15)
+						+商户名称(60)
+						+会员名称(60)
+						+交易时间(6)
+						+交易日期(8)
+						+交易单号(14)
+						+消费金额(12)
+						+账户余额(12)
+						+临时交易流水号（26）
+						以上都是定长，金额都是定长12位，前补0，其他不足位数后补空格
 
-  						* */
-  						tmp += "],商户代码=[" + ((_04_GetRecordReply) (rst)).getMer();//商户代码
-  						tmp += "],终端号=[" + ((_04_GetRecordReply) (rst)).getTmn();//终端号
-  						tmp += "],卡号=[" + ((_04_GetRecordReply) (rst)).getCardNo();//卡号
-  						tmp += "],交易批次号=[" + ((_04_GetRecordReply) (rst)).getTransacionBatchNo();//交易批次号
-  						tmp += "],原交易类型=[" + ((_04_GetRecordReply) (rst)).getTransacionVoucherNo();//原交易类型
-  						tmp += "],交易日期和时间=[" + ((_04_GetRecordReply) (rst)).getTransacionDatetime();//交易日期和时间
-  						tmp += "],交易金额=[" + ((_04_GetRecordReply) (rst)).getTransacionAmount();//交易金额
-  						tmp +="]";
-  						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 查询成功="+tmp,"com.txt");
+						* */
+						tmp += "],商户代码=[" + ((_04_GetRecordReply) (rst)).getMer();//商户代码
+						tmp += "],终端号=[" + ((_04_GetRecordReply) (rst)).getTmn();//终端号
+						tmp += "],卡号=[" + ((_04_GetRecordReply) (rst)).getCardNo();//卡号
+						tmp += "],交易批次号=[" + ((_04_GetRecordReply) (rst)).getTransacionBatchNo();//交易批次号
+						tmp += "],原交易类型=[" + ((_04_GetRecordReply) (rst)).getTransacionVoucherNo();//原交易类型
+						tmp += "],交易日期和时间=[" + ((_04_GetRecordReply) (rst)).getTransacionDatetime();//交易日期和时间
+						tmp += "],交易金额=[" + ((_04_GetRecordReply) (rst)).getTransacionAmount();//交易金额
+						tmp +="]";
+						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 返回成功="+tmp,"com.txt");
 
   						//退款参数获取
 						String tmp_spec = ((_04_GetRecordReply) (rst)).getSpecInfoField();
@@ -1476,24 +1534,29 @@ BushuoFragInteraction
 						if(tmp_spec!=null && tmp_spec_len>(2+19)){
 							rfd_card_no = (((_04_GetRecordReply) (rst)).getSpecInfoField()).substring(0+2,2+19).trim();
 						}
+						//【剩余金额】
+						if(tmp_spec!=null && tmp_spec_len>(2+19)){
+							cashbalance = (((_04_GetRecordReply) (rst)).getSpecInfoField()).substring((tmp_spec_len-26-12),(tmp_spec_len-26)).trim();
+						}
 						//【临时交易流水号】
 						if(tmp_spec!=null && tmp_spec_len>26){
 							rfd_spec_tmp_serial = (((_04_GetRecordReply) (rst)).getSpecInfoField()).substring((tmp_spec_len-26),tmp_spec_len);
 						}else{//使用空格时，表示上一次的【临时交易流水号】
 							rfd_spec_tmp_serial = String.format("%1$-26s","");
 						}
-						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 退款参数=金额"+amount+"卡号="+rfd_card_no+"流水号="+rfd_spec_tmp_serial,"com.txt");
-  						childmsg.what=CahslessTest.QUERYSUCCESS;
-  						childmsg.obj=((_04_GetRecordReply) (rst)).getSpecInfoField();
-  					}
-  					else
-  					{
-  						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 查询失败,code:"+rst.code+",info:"+rst.code_info,"com.txt");
-  						childmsg.what=CahslessTest.QUERYFAIL;
-  						childmsg.obj="";
-  					}
-  				}
-  				posmainhand.sendMessage(childmsg);
+						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 退款参数=金额"+amount+"卡号="+rfd_card_no+"流水号="+rfd_spec_tmp_serial+"剩余金额="+cashbalance,"com.txt");
+						childmsg.what=CahslessTest.QUERYSUCCESS;
+						childmsg.obj="返回成功="+tmp;
+					}
+					else
+					{
+						ToolClass.Log(ToolClass.INFO,"EV_COM","COMActivity 返回失败,code:"+rst.code+",info:"+rst.code_info,"com.txt");
+						cashbalance="";
+						childmsg.what=CahslessTest.QUERYFAIL;
+						childmsg.obj="返回失败";
+					}
+				}
+				posmainhand.sendMessage(childmsg);
   			}
   		}
 
